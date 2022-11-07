5f5ec1cffe31f5c7f476e35d03e1c4bedfacee42
sarah <sbenzi@furb.br>
2022-11-07 12:53:34 -0300

    anotacoes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 
 //  jsx = javascript + xml (xmp é o q tá por tras do html)
 
+// react nada mais é do que uma forma de vc dividir sua aplicacao em varias partes menores
+
 export default function Home() {
   return (
     <h1>hello nlw</h1>
