d9f445848847a448ce2225a82bcd067b890a988c
sarah benzi <sarahlover822@gmail.com>
2022-12-09 15:32:59 -0300

    definindo cor do body

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,14 +1,14 @@
 import {Html,Head,Main,NextScript} from 'next/document'
 
 export default function Document(){
-    return(
+    return (
         <Html>
             <Head>
             <link rel="preconnect" href="https://fonts.googleapis.com"/>
-            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin=''/>
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin = ""/>
             <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;1,700&family=Zilla+Slab+Highlight&display=swap" rel="stylesheet"/>
             </Head>
-            <body>
+            <body className=' bg-gray-900'>
                 <Main/>
                 <NextScript/>
             </body>
